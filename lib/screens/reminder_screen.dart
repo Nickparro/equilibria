f69daeb006b42d8a8f0473bf4271db245bd72a2c
julianp-1212 <Jaleparra@poligran.edu.co>
2025-06-08 12:45:08 -0500

    Update reminder_screen.dart

--- lib/screens/reminder_screen.dart
+++ lib/screens/reminder_screen.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class ReminderScreen extends StatelessWidget {
+class ReminderScreen extends StatefulWidget {
+  @override
+  _ReminderScreenState createState() => _ReminderScreenState();
+}
+
+class _ReminderScreenState extends State<ReminderScreen> {
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+  @override
+  void initState() {
+    super.initState();
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    const androidSettings = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const initializationSettings = InitializationSettings(android: androidSettings);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<void> _showNotification() async {
+    const androidDetails = AndroidNotificationDetails(
+      'channelId',
+      'channelName',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+    const notificationDetails = NotificationDetails(android: androidDetails);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      '¡Hora de una pausa!',
+      'Recuerda tomar un descanso o realizar una pausa activa.',
+      notificationDetails,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(title: Text('Gestión de Notificaciones')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _showNotification,
+          child: Text('Enviar recordatorio'),
+        ),
+      ),
     );
   }
 }
